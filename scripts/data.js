6b1fedf37cd88f842ec222b964a7e202a69e1688
Vlaqikk <ilinskyvladislav2002@mail.ru>
2024-01-09 16:35:41 +0500

    solve problem with images

--- scripts/data.js
+++ scripts/data.js
@@ -2,7 +2,7 @@ const containsItems = [
     {
         id: 0,
         head: 'Металлическая дверь',
-        image: '/assets/images/1.png',
+        image: 'assets/images/1.png',
         chance: 5,
         price: 5312,
         rarityColor: 'rgb(255, 221, 89)'
@@ -10,7 +10,7 @@ const containsItems = [
     {
         id: 1,
         head: 'Маска наказания',
-        image: '/assets/images/2.png',
+        image: 'assets/images/2.png',
         chance: 5,
         price: 3864,
         rarityColor: 'rgb(255, 221, 89)'
@@ -18,7 +18,7 @@ const containsItems = [
     {
         id: 2,
         head: 'Чужой Реликтовый Пистолет-пулемет',
-        image: '/assets/images/3.png',
+        image: 'assets/images/3.png',
         chance: 5,
         price: 1903,
         rarityColor: 'rgb(255, 221, 89)'
@@ -26,7 +26,7 @@ const containsItems = [
     {
         id: 3,
         head: 'Улыбашка',
-        image: '/assets/images/4.png',
+        image: 'assets/images/4.png',
         chance: 5,
         price: 1355,
         rarityColor: 'rgb(235, 65, 42)'
@@ -34,7 +34,7 @@ const containsItems = [
     {
         id: 4,
         head: 'Спальник ужаса',
-        image: '/assets/images/5.png',
+        image: 'assets/images/5.png',
         chance: 5,
         price: 1072,
         rarityColor: 'rgb(255, 57, 247)'
@@ -42,7 +42,7 @@ const containsItems = [
     {
         id: 5,
         head: 'Калаш фантом',
-        image: '/assets/images/6.png',
+        image: 'assets/images/6.png',
         chance: 25,
         price: 29,
         rarityColor: 'rgb(97, 167, 255)'
@@ -50,7 +50,7 @@ const containsItems = [
     {
         id: 6,
         head: 'Костяной нож Пайтити',
-        image: '/assets/images/7.png',
+        image: 'assets/images/7.png',
         chance: 50,
         price: 5,
         rarityColor: 'rgb(97, 167, 255)'
